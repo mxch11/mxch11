ad4714aebb1f9c17e75ad0671a9ebb46d7a48a9c
mxchi11 <papasfriasdemcdonalds@gmail.com>
2024-11-22 03:11:01 -0300

    html, views y urls principales

--- mv_app/urls.py
+++ mv_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('p_inicio/', views.p_inicio, name='p_inicio'),
+     path('d_inicio/', views.d_inicio, name='d_inicio'),
+      path('u_inicio/', views.p_inicio, name='u_inicio'),
 ]
--- mv_app/views.py
+++ mv_app/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'base.html')
+
+def p_inicio(request):
+    return render(request, 'servicios/pestañas/inicio.html')
+
+def d_inicio(request):
+    return render(request, 'servicios/depilacion/inicio.html')
+
+def u_inicio(request):
+    return render(request, 'servicios/uñas/inicio.html')
