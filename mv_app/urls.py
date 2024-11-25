8bfbef28be716105c6322f9b73d030defe921872
mxchi11 <papasfriasdemcdonalds@gmail.com>
2024-11-25 02:56:57 -0300

    x

--- mv_app/urls.py
+++ mv_app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index', views.index, name='index'),
     path('', views.landing_page, name='landing_page'),
+    path('index/', views.index, name='index'),
     path('p_inicio/', views.p_inicio, name='p_inicio'),
     path('d_inicio/', views.d_inicio, name='d_inicio'),
     path('u_inicio/', views.u_inicio, name='u_inicio'),
